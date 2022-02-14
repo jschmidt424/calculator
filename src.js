8cd297080f76eb3afa7d3f4f6610d7c32799a758
spazdrum <spazdrum@gmail.com>
2022-02-13 23:57:54 -0500

    Added Comments

--- src.js
+++ src.js
@@ -1,26 +1,34 @@
-let display = document.getElementById("display");
-let buttons = Array.from(document.getElementsByClassName("button"));
+let display = document.getElementById("display"); // save a reference to display dom element
+let buttons = Array.from(document.getElementsByClassName("button")); // save a reference to all buttons in an array
 
 buttons.map((button) => {
+  // loop through all buttons
   button.addEventListener("click", (e) => {
-    switch (e.target.innerText) {
-      case "C":
-        display.innerText = "";
-        break;
-      case "=":
+    // add event listener to each button
+    switch (
+      e.target.innerText // switch on the inner text of the button
+    ) {
+      case "C": // if the inner text is C
+        display.innerText = ""; // clear the display
+        break; // break out of the switch
+      case "=": // if the inner text is =
         try {
-          display.innerText = eval(display.innerText);
+          // try to parse the display
+          display.innerText = eval(display.innerText); // evaluate the display
         } catch {
-          display.innerText = "Error";
+          // if there is an error
+          display.innerText = "Error"; // set the display to error
         }
-        break;
-      case "←":
+        break; // break out of the switch
+      case "←": // if the inner text is ←
         if (display.innerText) {
-          display.innerText = display.innerText.slice(0, -1);
+          // if the display is not empty
+          display.innerText = display.innerText.slice(0, -1); // remove the last character from the display
         }
-        break;
+        break; // break out of the switch
       default:
-        display.innerText += e.target.innerText;
+        // if the inner text is not C, =, or ←
+        display.innerText += e.target.innerText; // add the inner text of the button to the display
     }
   });
 });
